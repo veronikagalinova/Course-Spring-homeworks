832d61350d24081059f2b19897f5a6db279a4f0b
Veronika Galinova <vgvalentinova@abv.bg>
2019-11-23 14:10:07 +0200

    Created project

---
+++ problem1/Blogger-Rest-API/src/main/java/bg/sofia/uni/fmi/Blogger/Rest/API/BloggerRestApiApplication.java
@@ -0,0 +1,13 @@
+package bg.sofia.uni.fmi.Blogger.Rest.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggerRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggerRestApiApplication.class, args);
+	}
+
+}
